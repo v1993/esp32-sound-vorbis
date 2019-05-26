3f50d2775ab75d8204020659cbb5a95abc7edaaf
v1993 <v19930312@gmail.com>
2019-05-26 11:44:58 +0300

    Fix traditional build

--- component.mk
+++ component.mk
@@ -1,6 +1,6 @@
 COMPONENT_SRCDIRS := . tremor ogg/src
 COMPONENT_ADD_INCLUDEDIRS := . config ogg/include
 #COMPONENT_PRIV_INCLUDEDIRS := config tremor ogg/include
-OGG_VORBIS_FLAGS = -include config/tremor/os.h -Wno-error=char-subscripts 
+OGG_VORBIS_FLAGS = -Wno-error=char-subscripts 
 CFLAGS += $(OGG_VORBIS_FLAGS)
 CXXFLAGS += $(OGG_VORBIS_FLAGS) -std=gnu++14
